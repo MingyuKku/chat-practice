a3154e7db9ddad2f9ad5b5f2acbdfb06a3760266
ganglabtol <ganglabtol@lab2ai.com>
2024-03-31 20:31:35 +0900

    히히

--- src/pages/chat/Chat.tsx
+++ src/pages/chat/Chat.tsx
@@ -42,8 +42,8 @@ const Chat = () => {
     } 
 
     return (
-        <div className='flex flex-col fixed top-0 left-0 w-full pt-basic-header pb-[64px] overflow-y-auto'>
-            <div className='h-viewScreen'>
+        <div className='flex flex-col fixed top-0 left-0 w-full pt-basic-header pb-[64px] overflow-y-auto overscroll-contain'>
+            <div className='h-viewScreen overscroll-contain'>
                 <MessageList
                     items={ messageItems.pages }
                 />
