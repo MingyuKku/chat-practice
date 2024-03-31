279abfd316203367947e606bec6718614fbb8204
ganglabtol <ganglabtol@lab2ai.com>
2024-03-31 21:04:23 +0900

    히히

--- src/App.tsx
+++ src/App.tsx
@@ -17,6 +17,21 @@ const App = () => {
   }, [])
 
 
+  const setScreenSize = () => {
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty("--vh", `${vh}px`); //"--vh"라는 속성으로 정의해준다.
+  }
+
+  React.useEffect(() => {
+        setScreenSize();
+        window.addEventListener('resize', setScreenSize);
+
+        return () => {
+            window.removeEventListener('resize', setScreenSize);
+        }
+    }, [])
+  
+
   return (
     <div className=''>
         <MainHeader />
